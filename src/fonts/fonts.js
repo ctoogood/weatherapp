5ac49f2afe25fc46c1278a6b7c555f99916b7b12
ctoogood <calumtoogood@gmail.com>
2020-02-07 23:34:17 +0100

    added fonts

--- src/fonts/fonts.js
+++ src/fonts/fonts.js
@@ -1,16 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
 import Bahnschrift from './bahnschrift.ttf';
-import FrostedCupcake from './frostedCupcake.ttf';
 
 export default createGlobalStyle`
-    @font-face {
-        font-family: 'Frosted Cupcake';
-        src: local('Frosted Cupcake'), local(FrostedCupcake),
-        url(${FrostedCupcake}) format('ttf');
-        font-weight: 400;
-        font-style: normal;
-    }
     @font-face {
         font-family: 'Bahnschrift';
         src: local('Bahnschrift'),
